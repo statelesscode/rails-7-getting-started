08201fe3adac26fb872199d2b2bc3020e9994ed3
Michael Duchemin <msducheminjr@users.noreply.github.com>
2021-12-21 23:33:48 -0500

    Generate the Article model and add to index view
    Add an Article model to match the previously created ArticlesController
    
    * Generate the Article model with title:string and body:text attributes.
    * Run the generated migration to produce the schema.rb file.
    * Change the default generated fixtures to be less boring
    * Modify the articles index action and view to query and display a list
      of articles
    * Update the ArticlesControllerTest with new functionality
    
    Completes Rails Getting Started Guide:
    * 6 MVC and You
      * 6.1 Generating a Model
      * 6.2 Database Migrations
      * 6.3 Using a Model to Interact with the Database
      * 6.4 Showing a List of Articles

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,6 +4,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get articles_url
     assert_response :success
-    assert_select 'h1', 'Hello, Rails! Taxation is theft!'
+    assert_select 'h1', 'Articles'
+    assert_select 'li', Article.count
   end
 end
