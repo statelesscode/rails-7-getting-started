c2debaee2ef5819ec661887dc0078b32adb299ae
Michael Duchemin <msducheminjr@users.noreply.github.com>
2021-12-23 17:33:25 -0500

    Add Articles show and fix fixtures
    Add the capability to show a single article and fix errors in multiline
    fixtures to make controller test assertsions pass.
    
    * Add the Articles show route
    * Add the show action to the ArticlesController
    * Add the show.html.erb view
    * Modify the index view to link to each article
    * Fix the fixtures to properly fold multiline text for the Article body
    
    Completes Rails Getting Started Guide:
    * 7 CRUDit Where CRUDit Is Due
      * 7.1 Showing a Single Article

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,10 +1,21 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @article = articles(:nerd)
+  end
+
   test "should get index" do
     get articles_url
     assert_response :success
     assert_select 'h1', 'Articles'
     assert_select 'li', Article.count
+  end
+
+  test "should get show" do
+    get "/articles/#{@article.id}"
+    assert_response :success
+    assert_select 'h1', @article.title
+    assert_select 'p', @article.body
   end
 end
