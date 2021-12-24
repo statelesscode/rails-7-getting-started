4a18941c41fc785aa1bf33838076e0ce039d759d
Michael Duchemin <msducheminjr@users.noreply.github.com>
2021-12-24 10:47:59 -0500

    Create articles in the app with a form
    Implement the new and create methods in the ArticlesController to allow
    for creation of a new article in the application using the
    new_article_path (articles/new) and a form in the created new.html.erb
    view file.
    
    * Add new and create actions to ArticlesController
    * Add private method article_params to meet Rails strong parameters
      requirement
    * Add tests for new and create in ArticlesControllerTest
    * Add new view with form_with helper
    
    Completes Rails Getting Started Guide:
    * 7 CRUDit Where CRUDit Is Due
      * 7.3 Creating a New Article
        * 7.3.1 Using a Form Builder
        * 7.3.2 Using Strong Parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
@@ -6,4 +7,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: "Article was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -18,4 +18,32 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h1', @article.title
     assert_select 'p', @article.body
   end
+
+  test "should get new" do
+    get new_article_url
+    assert_response :success
+    assert_select "h1", "New Article"
+    assert_select "form"
+    assert_select "form div", 3
+  end
+
+  test "should create article" do
+    article_title = "Sleepy Time"
+    article_body = "I'm asleep: #{'z' * 1000}"
+
+    assert_difference("Article.count") do
+      post articles_url, params: {
+        article: {
+          title: article_title,
+          body: article_body
+        }
+      }
+    end
+
+    article = Article.last
+    assert_redirected_to article_path(article)
+    assert_equal article_title, article.title
+    assert_equal article_body, article.body
+    assert_equal "Article was successfully created.", flash[:notice]
+  end
 end
