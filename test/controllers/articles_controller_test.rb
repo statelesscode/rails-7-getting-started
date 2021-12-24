3d76b4b82ac82ecc8a015950fa4eb963a51449db
Michael Duchemin <msducheminjr@users.noreply.github.com>
2021-12-24 11:51:03 -0500

    Add validations to Article title and body
    Add model validations and tests for the title and body attributes with
    content in the new article form to display any error messages inline if
    the form input is invalid. Also added a link to the new action in the
    articles index view.
    
    * Add title and body validations to Article model
    * Add model tests for happy and unhappy paths to ArticleTest
    * Add test for invalid form data to ArticleControllerTest
    * Add display of error messages to new.html.erb view
    * Add link to new_article_path to index.html.erb view
    
    Completes Rails Getting Started Guide:
    * 7 CRUDit Where CRUDit Is Due
      * 7.3 Creating a New Article
        * 7.3.3 Validations and Displaying Error Messages
        * 7.3.4 Finishing Up

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -3,6 +3,9 @@ require "test_helper"
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @article = articles(:nerd)
+    @title_blank = "Title can't be blank"
+    @body_blank = "Body can't be blank"
+    @body_short = "Body is too short (minimum is 10 characters)"
   end
 
   test "should get index" do
@@ -10,6 +13,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'h1', 'Articles'
     assert_select 'li', Article.count
+    assert_select 'a', 'New Article'
   end
 
   test "should get show" do
@@ -45,5 +49,23 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_equal article_title, article.title
     assert_equal article_body, article.body
     assert_equal "Article was successfully created.", flash[:notice]
+  end
+
+  test "should display errors if validations fail" do
+    assert_no_difference("Article.count") do
+      post articles_url, params: {
+        article: {
+          title: "",
+          body: ""
+        }
+      }
+    end
+
+    assert_select "h1", "New Article"
+    assert_select "form"
+    assert_select "div.error", 3
+    assert_select "div.error", @title_blank
+    assert_select "div.error", @body_blank
+    assert_select "div.error", @body_short
   end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,64 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @article = articles(:nerd)
+    @valid_title = "I have a valid title"
+    @valid_body = "This body is long enough."
+    @title_blank = "Title can't be blank"
+    @body_blank = "Body can't be blank"
+    @body_short = "Body is too short (minimum is 10 characters)"
+  end
+
+  test "should be valid and creatable with correct attributes" do
+    # new
+    article = Article.new(title: @valid_title, body: @valid_body)
+    assert article.valid?
+    assert article.save
+    article.reload
+    assert_not_nil article.id
+    assert_equal @valid_title, article.title
+    assert_equal @valid_body, article.body
+
+    # existing
+    assert @article.valid?
+  end
+
+  test "should be invalid if title is blank" do
+    # new
+    article = Article.new(body: @valid_body)
+    assert_not article.valid?
+    assert_includes article.errors.full_messages, @title_blank
+
+    # existing with nil
+    @article.title = nil
+    assert_not @article.valid?
+    assert_includes @article.errors.full_messages, @title_blank
+  end
+
+  test "should be invalid if body is blank" do
+    # new
+    article = Article.new(title: @valid_title)
+    assert_not article.valid?
+    assert_includes article.errors.full_messages, @body_blank
+
+    # existing with empty string
+    @article.body = ""
+    assert_not @article.valid?
+    assert_includes @article.errors.full_messages, @body_blank
+  end
+
+  test "should be invalid if body is too short" do
+    too_short = "Fail"
+    # new
+    article = Article.new(title: @valid_title, body: too_short)
+    assert_not article.valid?
+    assert_includes article.errors.full_messages, @body_short
+
+    # existing
+    @article.body = too_short
+    assert_not @article.valid?
+    assert_includes @article.errors.full_messages, @body_short
+  end
 end
