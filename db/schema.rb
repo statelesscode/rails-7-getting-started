c20c9a96bdd67cd5de8a447f464a40bd3a3a296b
Michael Duchemin <msducheminjr@users.noreply.github.com>
2021-12-29 17:24:13 -0500

    Add Comment model and nested route
    Add Comment model and migration with a foreign key reference to the
    Article model. Make the comment a nested resource under the Article
    resource so that each comment resource requires an :article_id parameter
    in the URL.
    
    Add has_many association in the Article model and include the dependent:
    :destroy option so that the Article destroy tests don't fail.
    
    Completes Rails Getting Started Guide:
    * 8 Adding a Second Model
      * 8.1 Generating a Model
      * 8.2 Associating Models
      * 8.3 Adding a Route for Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_200643) do
+ActiveRecord::Schema.define(version: 2021_12_29_220150) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -26,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_12_29_200643) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
