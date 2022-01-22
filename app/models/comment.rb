6c58c749c53355aafc8c7ddabfc28f28a4361c20
Michael Duchemin <msducheminjr@users.noreply.github.com>
2022-01-22 14:11:24 -0500

    Broadcast comment changes to parent article
    Use Turbo to broadcast any changes to the comments collection
    (belongs_to) on a given article. Add a browser test to validate that the
    changes work as intended.
    
    In order for the Turbo broadcast to work, the following criteria must be
    in place:
    
    * Add broadcasts_to in addition to belongs_to in the comment model
    * Articles show needs a dom_id for the artcile (added)
    * The comments collection needs to be in a DOM element with the ID of
      "comments" (added)
    * Each comment needs an element with its dom_id (already existed)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   include Visible
 
   belongs_to :article
+  broadcasts_to :article
 end
--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -3,22 +3,22 @@ require "application_system_test_case"
 class ArticlesTest < ApplicationSystemTestCase
   setup do
     @article = articles(:nerd)
+    @spam_commenter = "Social Media Giant"
+    @spam_body = "Visit the COVID-19 resource center so we can tell you what to think!"
   end
 
   test "destroying a comment" do
-    spam_commenter = "Social Media Giant"
-    spam_body = "Visit the COVID-19 resource center so we can tell you what to think!"
 
     visit construct_with_http_auth(edit_article_path(@article))
     visit article_url(@article)
     # add a spam comment
-    fill_in "Commenter", with: spam_commenter
-    fill_in "Body", with: spam_body
+    fill_in "Commenter", with: @spam_commenter
+    fill_in "Body", with: @spam_body
     # accept default for status
     click_on "Create Comment"
 
-    assert_text spam_commenter
-    assert_text spam_body
+    assert_text @spam_commenter
+    assert_text @spam_body
     within("div#comment_#{@article.comments.last.id}") do
       page.accept_confirm do
         click_on "Destroy Comment"
@@ -26,8 +26,8 @@ class ArticlesTest < ApplicationSystemTestCase
     end
     assert_text "Comment was successfully destroyed."
     assert_text @article.title
-    assert_no_text spam_commenter
-    assert_no_text spam_body
+    assert_no_text @spam_commenter
+    assert_no_text @spam_body
     assert_text "Comments"
     assert_text "Add a comment:"
   end
@@ -44,6 +44,41 @@ class ArticlesTest < ApplicationSystemTestCase
     assert_no_text @article.title
     assert_text articles(:why).title
     assert_text "New Article"
+  end
+
+  test "created comment broadcasts to article and adds to page" do
+    visit article_url(@article)
+
+    # validate that comment isn't in the DOM
+    assert_no_text @spam_commenter
+    assert_no_text @spam_body
+
+    # create a comment outside of the page context
+    @article.comments.create!(
+      commenter: @spam_commenter,
+      body: @spam_body,
+      status: "public"
+    )
+
+    # validate that comment gets inserted into the DOM
+    assert_text @spam_commenter
+    assert_text @spam_body
+  end
+
+  test "destroyed comment broadcasts to article and removes from page" do
+    visit article_url(@article)
+    comment = comments(:nerd_ruby)
+
+    # validate that comment is in the DOM
+    assert_text comment.commenter
+    assert_text comment.body
+
+    # destroy the comment outside of the page context
+    comment.destroy!
+
+    # validate that comment is no longer in the DOM
+    assert_no_text comment.commenter
+    assert_no_text comment.body
   end
 
   private
